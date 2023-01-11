0cc6afb12e9dfd816198759b65813f074180fe48
Chenhui Gu <c46gu@uwaterloo.ca>
2023-01-10 20:17:03 -0500

    fix cross port access and change db pool

--- backend/src/main/java/com/fellaverse/backend/controller/IndexController.java
+++ backend/src/main/java/com/fellaverse/backend/controller/IndexController.java
@@ -1,2 +1,15 @@
-package com.fellaverse.backend.controller;public class IndexController {
+package com.fellaverse.backend.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class IndexController {
+
+    @GetMapping(value={"/","/index"})
+    @ResponseBody
+    public String index() {
+        return "Hello, fellas";
+    }
 }
