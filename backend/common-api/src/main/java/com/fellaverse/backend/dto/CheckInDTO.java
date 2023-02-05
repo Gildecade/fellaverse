d8c52d385950c5615ff0148eaf9a643200fd59fc
Yi Chiu <y23chiu@uwaterloo.ca>
2023-02-05 11:10:23 -0500

    check in DTO

---
+++ backend/common-api/src/main/java/com/fellaverse/backend/dto/CheckInDTO.java
@@ -0,0 +1,27 @@
+package com.fellaverse.backend.dto;
+
+import com.fellaverse.backend.Validator.ValidGroup;
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class CheckInDTO {
+    @Null(groups = ValidGroup.Crud.Create.class, message = "CheckIn ID should be null when creating")
+    @NotNull(groups = {ValidGroup.Crud.Update.class, ValidGroup.Crud.Delete.class}, message = "CheckIn ID cannot be null")
+    private Long id;
+
+    @NotNull(groups = {ValidGroup.Crud.Create.class, ValidGroup.Crud.Update.class, ValidGroup.Crud.Delete.class}, message = "User ID cannot be null")
+    private Long userId;
+
+    @NotNull(groups = {ValidGroup.Crud.Create.class, ValidGroup.Crud.Update.class}, message = "Start date time cannot be null")
+    private LocalDateTime startDateTime;
+
+    @NotNull(groups = {ValidGroup.Crud.Create.class, ValidGroup.Crud.Update.class}, message = "End date time cannot be null")
+    private LocalDateTime endDateTime;
+
+    private Float weight;
+
+}
+
