ec4cc1f59237593d5f3d1a7d9decead03ab8cd3c
Chenhui Gu <c46gu@uwaterloo.ca>
2023-03-06 22:15:46 -0500

    added length check

--- backend/common-api/src/main/java/com/fellaverse/backend/dto/AdminDTO.java
+++ backend/common-api/src/main/java/com/fellaverse/backend/dto/AdminDTO.java
@@ -16,17 +16,21 @@ public class AdminDTO implements Serializable {
     private Long id;
 
     @NotBlank(groups = ValidGroup.Crud.Create.class, message = "Username cannot be blank")
+    @Size(max = 60)
     private String username;
 
     @NotBlank(groups = ValidGroup.Crud.Create.class, message = "Password cannot be blank")
+    @Size(max = 60)
     private String password;
 
     @NotBlank(groups = ValidGroup.Crud.Create.class, message = "Email cannot be blank")
     @Email(message = "Please enter correct Email address")
+    @Size(max = 255)
     private String email;
 
     @NotBlank(groups = ValidGroup.Crud.Create.class, message = "Phone number cannot be blank")
     @Pattern(regexp = "^(\\+\\d{1,2}\\s)?\\(?\\d{3}\\)?[\\s.-]?\\d{3}[\\s.-]?\\d{4}$", message = "Phone number format is wrong")
+    @Size(max = 60)
     private String phoneNumber;
 
     private List<Long> roleIds;
